2dd38fa4799432e7f37762fe78acb5e007032beb
Paul <paultanase2@gmail.com>
2025-01-15 18:12:50 -0500

    First commit

--- apps/angular-store/src/app/app.routes.ts
+++ apps/angular-store/src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Route } from '@angular/router';
+import { NxWelcomeComponent } from './nx-welcome.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+        path: 'home',
+        component: NxWelcomeComponent,
+    },
+    {
+        path: 'products',
+        loadComponent: () => import('@angular-monorepo/products').then((m) => m.ProductsComponent),
+    },
+  {
+    path: 'orders',
+    loadComponent: () =>
+      import('@angular-monorepo/orders').then((m) => m.OrdersComponent),
+  },
+  {
+    path: '',
+    component: NxWelcomeComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    component: NxWelcomeComponent,
+  },
+];
---
+++ apps/inventory-e2e/src/example.spec.ts
@@ -0,0 +1,5 @@
+import { test, expect } from '@playwright/test';
+
+test('1+1=2', async () => {
+  expect(1+1).toBe(2);
+});
